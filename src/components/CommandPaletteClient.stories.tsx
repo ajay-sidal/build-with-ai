7d4202de8467574acaf1a4f7ba189b7882a1f77e
Ajay Sidal <ajay.sidal@opsvantagedigital.online>
2026-02-24 15:22:21 +1300

    Remove Storybook stories and test artifacts from production build

--- src/components/CommandPaletteClient.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import CommandPaletteClient from './CommandPaletteClient'
-
-export default {
-  title: 'Components/CommandPalette',
-  component: CommandPaletteClient,
-}
-
-export const Default = () => <CommandPaletteClient />
